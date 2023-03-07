c9f09946d072f4b20a28f5d4261dee78d0fa7e87
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-07 13:22:19 -0300

    Added ToMM to the fake pcbnew.py

--- pcbnew.py
+++ pcbnew.py
@@ -11,5 +11,8 @@ Edge_Cuts = 0
 def FromMM(val):
     return 0
 
+def ToMM(val):
+    return 0
+
 F_Cu = B_Cu = F_Adhes = B_Adhes = F_Paste = B_Paste = F_SilkS = B_SilkS = F_Mask = B_Mask = Dwgs_User = 0
 Cmts_User = Eco1_User = Eco2_User = Edge_Cuts = Margin = F_CrtYd = B_CrtYd = F_Fab = B_Fab = 0
